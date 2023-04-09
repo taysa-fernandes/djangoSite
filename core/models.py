fef7329f27ccf5c01b815d94719a0bbec99037ff
taysa-fernandes <taysamirelly34@gmail.com>
2023-04-09 14:16:04 -0300

    projeto finalizado

---
+++ core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Produto(models.Model):
+    nome= models.CharField('Nome',max_length=100)
+    preco= models.DecimalField('Preço',decimal_places=2, max_digits=8)
+    estoque= models.IntegerField('Quantidade em estoque')
+    def __str__(self):
+        return self.nome
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome',max_length=100)
+    sobrenome = models.CharField('sobrenome',max_length=100)
+    email = models.EmailField('email',max_length=100)
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome}'
